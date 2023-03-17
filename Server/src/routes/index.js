6fd104b5d34b35d4bb0e6d8335edc47e74ea64c2
BaltaKlixJones <balta98_@hotmail.com>
2023-03-17 10:09:44 -0300

    First commit

---
+++ Server/src/routes/index.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+// // Importar todos los routers;
+// // Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+// const turnRouter = require("./TurnRouter.js");
+// const serviceRouter = require("./ServiceRouter.js");
+// const professionalRouter = require("./ProfessionalRouter.js");
+// const clientRouter = require("./ClientRouter.js");
+// const paymentRouter = require("./PaymentRouter.js");
+// const reviewRouter = require("./ReviewRouter.js")
+
+
+// // Configurar los routers
+// // Ejemplo: router.use('/auth', authRouter);
+
+// router.use("/turn", turnRouter);
+// router.use("/service", serviceRouter);
+// router.use("/professional", professionalRouter);
+// router.use("/client", clientRouter);
+// router.use("/payment", paymentRouter);
+// router.use("/review", reviewRouter)
+
+
+module.exports = router;
