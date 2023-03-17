698a14d5cf6a5b6e732bae338e378fdab8bae52a
AndresPM98 <andresperez_98@hotmail.com>
2023-03-17 13:01:39 -0300

    srevicios

---
+++ Server/src/routes/ServiceRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const {
+  getServiceHandler,
+  getByIdServiceHandler,
+  deleteServiceHandler,
+  postServiceHandler,
+  putServiceHandler,
+} = require("../Handlers/ServiceHandler");
+const serviceRouter = Router();
+
+serviceRouter.get("/", getServiceHandler);
+serviceRouter.get("/:id", getByIdServiceHandler);
+serviceRouter.delete("/:id", deleteServiceHandler);
+serviceRouter.post("/", postServiceHandler);
+serviceRouter.put("/:id", putServiceHandler);
+
+module.exports = serviceRouter;
