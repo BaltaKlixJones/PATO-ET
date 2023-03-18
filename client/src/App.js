72f8fd0bb5c6a080aed96168b94510d34a19e9b5
BaltaKlixJones <balta98_@hotmail.com>
2023-03-18 17:16:12 -0300

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import React from "react"
 import './App.css';
+
+import { Switch, Route } from "react-router-dom";
+import Head from "./Pages/Head/Head";
+
 
 function App() {
   return (
     <div className="App">
+     
+      <Head/>    
      
     </div>
   );
