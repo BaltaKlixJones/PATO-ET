ad9f4e1e4c673d18cf1fdebb4f5e77a808799b53
AndresPM98 <andresperez_98@hotmail.com>
2023-03-18 20:10:03 -0300

    a ver si te anda ahora

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 import './App.css';
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 
 import Head from "./Pages/Head/Head";
-import Home from "./Pages/Home/Home";
+import {Home} from "./Pages";
 import NavBar from "./Components/NavBar/NavBar";
 
 
@@ -13,9 +13,9 @@ function App() {
     <div className="App">
       <Head/>
       <NavBar/>
-     <Routes>
-      <Route exact path="/" element={<Home/>} />
-    </Routes>
+     <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
     </div>
   );
 }
