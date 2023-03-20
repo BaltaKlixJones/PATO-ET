794f1a3ead46bd0fd131a7e9e7428d07b39d7c42
AndresPM98 <andresperez_98@hotmail.com>
2023-03-20 09:43:31 -0300

    Css de Cards

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -6,12 +6,14 @@ const Card = ({ id, name, lastName, profession, description, image }) => {
   return (
     <div className={style.card}>
       {/* <div className={style.image}>{image}</div> */}
-      <img src={image} alt="image" />
+      <img src={image} className={style.cardImage} alt="image" />
       <div className={style.name}>{name}</div>
       <div className={style.lastname}>{lastName}</div>
       <div className={style.profession}>{profession}</div>
       <div className={style.description}>{description}</div>
-    </div>
+     
+</div>
+   
   );
 };
 
