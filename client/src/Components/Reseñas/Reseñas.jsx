bdd819384a143508d37359bfec753a8449d26834
BaltaKlixJones <balta98_@hotmail.com>
2023-03-22 11:05:31 -0300

    incio

--- client/src/Components/Reseñas/Reseñas.jsx
+++ client/src/Components/Reseñas/Reseñas.jsx
@@ -2,37 +2,38 @@ import "./Reseñas.css"
 
 export default function Reseñas() {
     return (
-        <div>
-        <div class="reseñas">
-          <h1>Opiniones de Usuarios</h1>
+        <div className="container" >
+        <div className="reseñas">
+          <h1 style={{marginLeft:"-1300px"}}>Algunas opiniones</h1>
+          <p style={{marginLeft:"-980px"}}>¡ Nos importa mucho las opiniones de nuestro centro para poder seguir mejorando !</p>
       
-          <div class="container"> 
-            <div class="reseña">
+          <div className="container"> 
+            <div className="reseña">
               <h2>Micaela Valdez</h2>
               <p>"Excelentes profesionales con gran calidad humana."</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
             </div>
-            <div class="reseña">
+            <div className="reseña">
               <h2>Diego Portal</h2>
               <p>"Mi hija asiste ahi. Son excelentes en todas las areas. Estamos felices. Super recomendados."</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
             </div>
-            <div class="reseña">
+            <div className="reseña">
               <h2>Ely CM</h2>
               <p>"Centro de Estimulación temprana! Realizan un trabajo excelente, nosotros vamos hace 2 años y mi hijo le encanta! Excelentes profesionales!!"</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
             </div>
-            <div class="reseña">
+            <div className="reseña">
               <h2>Gabriel Zalazar</h2>
               <p>"Hermoso lugar y excelentes profesionales."</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
             </div>
-            <div class="reseña">
+            <div className="reseña">
               <h2>Nicolas Bf</h2>
               <p>"Excelentes intalaciones con un equipamiento muy completo. El equipo humano es maravilloso y muy capacitado. Sin duda lo recomiendo."</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
             </div>
-            <div class="reseña">
+            <div className="reseña">
               <h2>María soledad Guasco</h2>
               <p>"Cuenta con los mejores profesionales de Salta y tiene lo último en equipamiento!"</p>
               <p>⭐️⭐️⭐️⭐️⭐️</p>
