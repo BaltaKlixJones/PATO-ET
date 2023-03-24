7652b87096278435d1f754784aa2127a9cd72747
BaltaKlixJones <balta98_@hotmail.com>
2023-03-24 11:42:14 -0300

    inicio responsive

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -4,32 +4,34 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className={style.navbar} style={{ textDecoration: "none" }}>
+    <div className={style.navbar}> 
+    <div className={style.menu}>
       <Link to="/">
         <button className={style.bottInicio}>Inicio</button>
       </Link>
-
+  
       <Link to="/Centro">
         <button className={style.bottProf}>Centro</button>
       </Link>
-
+  
       <Link to="/Blog">
         <button className={style.bottQS}>Blog</button>
       </Link>
-
+  
       <Link to="/QuienSoy">
         <button className={style.bottQS}>Quien soy</button>
       </Link>
-
+  
       <Link to="/Cursos">
         <button className={style.bottCursos}>Cursos</button>
       </Link>
-
+  
       <Link to="/Contacto">
         <button className={style.bottContacto}>Contacto</button>
       </Link>
     </div>
-  );
+  </div>
+  )
 };
 
 export default NavBar;
