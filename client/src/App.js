ceff188b5aaefba1615c3e4d0cd8340efc377805
BaltaKlixJones <balta98_@hotmail.com>
2023-04-10 18:46:39 -0300

    pagina admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,51 @@
 import React from "react"
 import './App.css';
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, useLocation, Redirect } from "react-router-dom";
 import {Home, Location, Centro, Head, Admin, Contacto} from "./Pages";
 import NavBar from "./Components/NavBar/NavBar";
 import Reseñas from "./Components/Reseñas/Reseñas";
 import Blog from "./Components/Blog/Blog";
+import Form from "./Components/Form/Form";
 import Cursos from "./Pages/Cursos/Cursos";
 import Descripcion from "./Pages/Descripcion/Descripcion";
 import QuienSoy from "./Pages/Info/QuienSoy";
 import axios from "axios"; 
-axios.defaults.baseURL = "https://patoet-back-production.up.railway.app/"; 
+import { BD } from "./config";
+axios.defaults.baseURL = BD; 
 
 
 function App() {
 
+  const location = useLocation();
   
 
   return (
     <div className="App">
-     <BrowserRouter>
-      <Head/>
-      <NavBar/>
-      <Route exact path="/" render={(props) => (
-        <>
-          <Home {...props} />
-         <Descripcion {...props} />
-          <Reseñas {...props} />
-        </>
-      )} />
-      {/* <Route exact path="/Admin" render={(props) => (
-        <>
-          <Admin {...props} />
-          <Location {...props} />
-          <Reseñas {...props} />
-        </>
-      )} /> */}
-      <Route exact path="/Centro" component={Centro} />
-      <Route exact path="/Blog" component={Blog} />
-      <Route exact path="/Contacto" component={Contacto} />
-      <Route exact path="/Cursos" component={Cursos} />
-      <Route exact path="/QuienSoy" component={QuienSoy} />
-
-    </BrowserRouter>
+      <BrowserRouter>
+      {location.pathname.startsWith('/Admin') ? (
+  <Head />
+) : (
+  <>
+    <Head />
+    <NavBar />
+  </>
+)}
+      
+        <Route exact path="/" render={(props) => (
+          <>
+            <Home {...props} />
+            <Descripcion {...props} />
+            <Reseñas {...props} />
+          </>
+        )} />
+        <Route exact path="/Admin" component={Form} />
+         <Route exact path="/Administrador/:id" component={Admin} />
+        <Route exact path="/Centro" component={Centro} />
+        <Route exact path="/Blog" component={Blog} />
+        <Route exact path="/Contacto" component={Contacto} />
+        <Route exact path="/Cursos" component={Cursos} />
+        <Route exact path="/QuienSoy" component={QuienSoy} />
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Pages/Admin/Admin.jsx
+++ client/src/Pages/Admin/Admin.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import style from"./Admin.module.css";
+import style from "./Admin.module.css";
 import inicio from "../../Imagenes/inicio.png";
 import Location from "../Location /Location";
 import Reseñas from "../../Components/Reseñas/Reseñas";
-
+import { getProfessionals } from "../../Redux/Actions";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 export default function Admin() {
+  const allProfessionals = useSelector((state) => state.professionals);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProfessionals());
+  }, [dispatch]);
+
   return (
     <div className={style.admin}>
-      <div className={style.imagenAdmin}>
-        <img src={inicio} alt="" />
-      </div>
       <div className={style.tituloAdmin}>
-        <h1>HOLA</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-          dolores, harum accusantium delectus sapiente nihil veritatis ducimus
-          illo quas exercitationem in ullam distinctio hic, repudiandae
-          reiciendis cupiditate debitis cum ipsum!
-        </p>
+        <div>
+
+        <button className={style.btncerrarsesion}>Cerrar sesion</button>
+        </div>
+        <h1>PANEL DE ADMINISTRADORA</h1>
+        <h4>
+          Bienvenida PATO!
+          <br />
+        </h4>
+        Este es el panel de administradora, donde podras eliminar profesionales
+        y editar fotos
+        <div className={style.btn}>
+          
+          <button className={style.button}>Profesionales</button>
+          <button className={style.button}>Blog</button>
+          <button className={style.button}>Contacto</button>
+        </div>
       </div>
     </div>
   );
